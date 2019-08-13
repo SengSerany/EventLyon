b58cf81c458fb448f646f4c1abaa91a57832bc90
SengSerany <cranyseng@gmail.com>
2019-08-13 16:18:58 +0200

    add mailjet

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
-
+gem 'mailjet'
 gem 'devise'
 gem 'dotenv-rails'
 gem 'letter_opener'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+	  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 	has_many :attendances, foreign_key: 'attendant_id', class_name: "Attendance"
 	has_many :events, foreign_key: 'admin_id', class_name: "Event"
 
